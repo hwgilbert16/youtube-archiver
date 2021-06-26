b23ea0dba9619c99c10c0d2444bd043bfa1ff256
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-06-26 19:12:30 -0400

    groundwork done - filtering video response

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const filterObject = require('filter-obj');
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'pug');
 
+async function getVideoInfo(url) {
+    const info = await ytdl.getBasicInfo(url);
+    return info;
+}
+
 app.get('/', (req, res) => {
-    res.send('<form action="/" method="post"><input type="text" name="username"><input type="submit"></form>');
+    res.render('index');
 });
+
+app.post('/save', (req, res) => {
+    const url = req.body.videoURL;
+
+    getVideoInfo(url).then((info) => {
+        let qualityOptions = info.player_response.streamingData.adaptiveFormats;
+
+        for (let i = 0; i < qualityOptions.length; i++) {
+            const filtered = filterObject(qualityOptions[i], ['mimeType']);
+            console.log(filtered);
+        }
+
+        res.send('successful');
+    });
+
+})
 
 app.listen(3000);
