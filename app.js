2ba0ae03c2685075fda8dcd12c21d5c4925b5832
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-06-26 19:48:53 -0400

    add comments

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 const filterObject = require('filter-obj');
 
+// middleware for getting post data from the body in req
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'pug');
 
+// render root page
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -16,19 +18,22 @@ app.post('/save', (req, res) => {
     const url = req.body.videoURL;
 
     ytdl.getBasicInfo(url).then((info) => {
+        // get the adaptiveformats section from the getBasicInfo response
         let qualityOptionsRaw = info.player_response.streamingData.adaptiveFormats;
         let qualityOptions = [];
 
+        // loop through each quality option to filter
         for (let i = 0; i < qualityOptionsRaw.length; i++) {
             const filtered = filterObject(qualityOptionsRaw[i], ['mimeType', 'qualityLabel']);
 
+            // check if it is mp4
             if (filtered.mimeType.includes('video/mp4')) {
                 qualityOptions.push(filtered.qualityLabel);
             }
 
         }
-        console.dir(qualityOptions);
 
+        // temporary response to prevent browser from hanging
         res.send('successful');
     });
 
