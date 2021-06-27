0169a7e31fa2008549f34181e80d206bf6f27d5e
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-06-27 15:42:47 -0400

    add loading gif and quality selection buttons

--- public/search.js
+++ public/search.js
@@ -5,33 +5,45 @@ $('#search').submit((e) => {
         'videoURL': $('#search').serializeArray()[0].value
     };
 
-    console.log(formData)
+    // ajax post request for video info
     $.ajax({
         type: 'POST',
         data: JSON.stringify(formData),
         contentType: 'application/json',
         dataType: 'json',
         url: '/search',
-        beforeSend: () => {
-            console.log('hello!');
-        },
+        beforeSend: addLoadingGif,
         success: renderReceivedData
+    }).done(() => {
+        // removes loading gif once data is received
+        $('#loadingGif').remove();
     })
 });
 
+// renders received data from server
 function renderReceivedData(data) {
     const img = new Image(640, 360)
     img.src = data.videoThumbnail;
-
     const title = document.createElement('p');
     title.textContent = data.videoTitle;
-
     const author = document.createElement('p');
     author.textContent = data.videoAuthor;
-
     $("#results").append(img, title, author);
+
+    console.dir(data);
+
+    for (let i = 0; i < data.videoQualityOptions.length; i++) {
+        const button = document.createElement('button');
+        button.textContent = data.videoQualityOptions[i].qualityLabel;
+        $('#qualityList').append(button);
+    }
 }
 
+// renders loading gif while video info is loading
 function addLoadingGif() {
+    const loadingGif = new Image();
+    loadingGif.src = 'loading.gif';
+    loadingGif.setAttribute('id', 'loadingGif');
 
+    $("#results").append(loadingGif);
 }
